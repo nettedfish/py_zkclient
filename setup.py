c4bb7bb1ee4a83c319941248de7be87b225e493b
yong.li <liyong19861014@gmail.com>
2013-12-24 11:02:57 +0800

    add setup.py ignore build dir

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='zkclient',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/airhuman/py_zkclient.git',
+    license='',
+    author='Michael',
+    author_email='liyong19861014@gmail.com',
+    description=''
+)
